b8f9e285c8f04529484c1808de03fe595c12cea9
Dmitry <verchyk91@mail.ru>
2021-02-12 20:06:04 +0300

    Размер изображения

--- shop/mainapp/admin.py
+++ shop/mainapp/admin.py
@@ -1,30 +1,52 @@
-from django import forms
+from PIL import Image
+
+from django.forms import ModelChoiceField, ModelForm, ValidationError
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import *
 
 
-class NotebookCategoryChoiceField(forms.ModelChoiceField):
-    pass
+class NotebookAdminForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = mark_safe(
+            """<span style="color:red; font-size:14px;">При загрузке изображения с разрешением больше {}x{} оно будет обрезано!</span>
+            """.format(
+                *Product.MIN_RESOLUTION
+            )
+        )
+
+    def clean_image(self):
+        image = self.cleaned_data['image']
+        img = Image.open(image)
+        min_height, min_width = Product.MIN_RESOLUTION
+        max_height, max_width = Product.MAX_RESOLUTION
+        if image.size > Product.MAX_IMAGE_SIZE:
+            raise ValidationError('Размер изображения не должен превышать 3 MB')
+        if img.height < min_height or img.width < min_width:
+            raise ValidationError('Разрешение изображения меньше минимального!')
+        if img.height > max_height or img.width > max_width:
+            raise ValidationError('Разрешение изображения больше максимального!')
+        return image
 
 
 class NotebookAdmin(admin.ModelAdmin):
 
+    form = NotebookAdminForm
+
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
-            return NotebookCategoryChoiceField(Category.objects.filter(slug='notebooks'))
+            return ModelChoiceField(Category.objects.filter(slug='notebooks'))
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
-
-
-class SmartphoneCategoryChoiceField(forms.ModelChoiceField):
-    pass
 
 
 class SmartphoneAdmin(admin.ModelAdmin):
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
-            return NotebookCategoryChoiceField(Category.objects.filter(slug='smartphones'))
+            return ModelChoiceField(Category.objects.filter(slug='smartphones'))
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -1,9 +1,48 @@
+import sys
+from PIL import Image
+
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.core.files.uploadedfile import InMemoryUploadedFile
+
+from io import BytesIO
 
 User = get_user_model()
+
+
+class MinResolutionErrorException(Exception):
+    pass
+
+
+class MaxResolutionErrorException(Exception):
+    pass
+
+
+class LatestProductsManager:
+
+    @staticmethod
+    def get_products_for_main_page(*args, **kwargs):
+        with_respect_to =kwargs.get('with_respect_to')
+        products = []
+        ct_models = ContentType.objects.filter(model__in=args)
+        for ct_model in ct_models:
+            model_products = ct_model.model_class()._base_manager.all().order_by('-id')[:5]
+            products.extend(model_products)
+        if with_respect_to:
+            ct_model = ContentType.objects.filter(model=with_respect_to)
+            if ct_model.exists():
+                if with_respect_to in args:
+                    return sorted(
+                        products, key=lambda x: x.__class__._meta.model_name.startswith(with_respect_to), reverse=True
+                    )
+        return products
+
+
+class LatestProducts:
+
+    objects = LatestProductsManager()
 
 
 class Category(models.Model):
@@ -17,6 +56,10 @@ class Category(models.Model):
 
 class Product(models.Model):
 
+    MIN_RESOLUTION = (400, 400)
+    MAX_RESOLUTION = (800, 800)
+    MAX_IMAGE_SIZE = 3145728
+
     class Meta:
         abstract = True
 
@@ -29,6 +72,28 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+    #     image = self.image
+    #     img = Image.open(image)
+    #     min_height, min_width = self.MIN_RESOLUTION
+    #     max_height, max_width = self.MAX_RESOLUTION
+    #     if img.height < min_height or img.width < min_width:
+    #         raise MinResolutionErrorException('Разрешение изображения меньше минимального!')
+    #     if img.height > max_height or img.width > max_width:
+    #         raise MaxResolutionErrorException('Разрешение изображения больше максимального!')
+        image = self.image
+        img = Image.open(image)
+        new_img = img.convert('RGB')
+        resized_new_img = new_img.resize((200, 200), Image.ANTIALIAS)
+        filestream = BytesIO()
+        resized_new_img.save(filestream, 'JPEG', quality=90)
+        filestream.seek(0)
+        name = '{}.{}'.format(*self.image.name.split('.'))
+        self.image = InMemoryUploadedFile(
+            filestream, 'ImageField', name, 'jpeg/image', sys.getsizeof(), None
+        )
+        super().save(*args, **kwargs)
 
 
 class Notebook(Product):
